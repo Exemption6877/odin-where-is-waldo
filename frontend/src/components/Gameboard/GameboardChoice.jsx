4a35cd31fc84e80938f8bd71e85aa674872bda6e
Exemption6877 <106035951+Exemption6877@users.noreply.github.com>
2025-07-12 06:36:48 +0300

    Style gameboard choicer

--- frontend/src/components/Gameboard/GameboardChoice.jsx
+++ frontend/src/components/Gameboard/GameboardChoice.jsx
@@ -1,17 +1,34 @@
 import { Link } from "react-router-dom";
+import styles from "./GameboardChoice.module.css";
 
 // Will fetch API for all options here later.
+// Put original link in db as well
 
 function GameboardChoice() {
   return (
-    <ul>
+    <ul className={styles.choiceWrapper}>
       <li>
+        <img src={`/assets/gameboard-preview/gm-1-preview.jpg`} alt="" />
+        <p>
+          By: u/Chekavo{" "}
+          <a href="https://www.reddit.com/r/wimmelbilder/comments/d8lioz/ad_2222_character_poster_oc_finaly_done/">
+            (Link)
+          </a>
+        </p>
         <Link to="/game/1">Gameboard 1</Link>
       </li>
       <li>
+        <img src={`/assets/gameboard-preview/gm-2-preview.jpeg`} alt="" />
+        <p>
+          By: <a href=""></a>
+        </p>
         <Link>Gameboard 2</Link>
       </li>
       <li>
+        <img src={`/assets/gameboard-preview/gm-3-preview.jpeg`} alt="" />
+        <p>
+          By: <a href=""></a>
+        </p>
         <Link>Gameboard 3</Link>
       </li>
     </ul>
