2ae567a0860590cf294e6ac766a6dc2e25f43241
Exemption6877 <106035951+Exemption6877@users.noreply.github.com>
2025-07-12 20:28:27 +0300

    Make objectives draggable

--- frontend/src/components/Gameboard/GameObjectives.jsx
+++ frontend/src/components/Gameboard/GameObjectives.jsx
@@ -1,15 +1,20 @@
 import styles from "./GameObjectives.module.css";
+import Draggable from "react-draggable";
+import { useRef } from "react";
 
 function GameObjectives({ objectives }) {
+  const nodeRef = useRef(null);
   return (
-    <div className={styles.objectivesWrapper}>
-      {objectives.map((objective) => (
-        <div key={objective.index} className={styles.objectiveWrapper}>
-          <p>{objective.name}</p>
-          <img src={`${objective.image}`} />
-        </div>
-      ))}
-    </div>
+    <Draggable nodeRef={nodeRef} bounds="parent">
+      <div ref={nodeRef} className={styles.objectivesWrapper}>
+        {objectives.map((objective) => (
+          <div key={objective.index} className={styles.objectiveWrapper}>
+            <p>{objective.name}</p>
+            <img src={`${objective.image}`} />
+          </div>
+        ))}
+      </div>
+    </Draggable>
   );
 }
 
