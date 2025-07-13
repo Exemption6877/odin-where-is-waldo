bc63ab9a343f53ebe010f334154b17c95ef0113e
Exemption6877 <106035951+Exemption6877@users.noreply.github.com>
2025-07-13 07:38:21 +0300

    Refactor gameboard upload route/controller

--- backend/controllers/adminController.js
+++ backend/controllers/adminController.js
@@ -2,33 +2,13 @@ const db = require("../prisma/queries");
 const supabase = require("../supabase/queries");
 const path = require("path");
 
-async function postGameboard(req, res) {
-  try {
-    const parsingData = {
-      title: "test",
-      image: "testURL",
-      preview: "testURL",
-      author: "someguy",
-      source: "somewhere",
-    };
-
-    await db.admin.addGameboard(parsingData);
-    res.status(200).json({ message: "Gameboard added successfully." });
-  } catch (err) {
-    console.error(err);
-    res.status(500).json({ error: "Could not add new gameboard." });
-  }
-}
-
-async function postUpload(req, res) {
+async function postUploadGameboard(req, res) {
   try {
     const { title, author, source } = req.body;
 
     const gameboard = req.files.gameboard[0];
     const preview = req.files.preview[0];
-
-    const counter = await db.admin.countGameboards();
-    const nextId = counter + 1;
+    const nextId = (await db.admin.countGameboards()) + 1;
 
     const gameboardPath =
       `gameboard_${nextId}` + path.extname(gameboard.originalname);
@@ -57,8 +37,8 @@ async function postUpload(req, res) {
     res.status(200).json({ message: "Files uploaded successfully." });
   } catch (err) {
     console.error(err);
-    res.status(500).json({ error: err });
+    res.status(500).json({ message: "Could not add new gameboard." });
   }
 }
 
-module.exports = { postGameboard, postUpload };
+module.exports = { postUploadGameboard };
--- backend/routers/adminRouter.js
+++ backend/routers/adminRouter.js
@@ -5,15 +5,16 @@ const upload = multer({ storage: multer.memoryStorage() });
 const adminController = require("../controllers/adminController");
 const adminRouter = Router();
 
-adminRouter.post("/gameboard", adminController.postGameboard);
 adminRouter.post(
-  "/uploadtest",
+  "/gameboard",
 
   upload.fields([
     { name: "gameboard", maxCount: 1 },
     { name: "preview", maxCount: 1 },
   ]),
-  adminController.postUpload
+  adminController.postUploadGameboard
 );
+
+// adminRouter.post("/objective", upload.single("objective"));
 
 module.exports = adminRouter;
